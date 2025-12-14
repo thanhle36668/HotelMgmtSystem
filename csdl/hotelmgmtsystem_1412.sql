1577e1ef3f93246668bc24057a3391c250285c8d
Thành Lê <thanhle36668@gmail.com>
2025-12-14 11:51:11 +0700

    CSDL 14/12/2025

---
+++ csdl/hotelmgmtsystem_1412.sql
@@ -0,0 +1,326 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2025 at 06:46 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hotelmgmtsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billing`
+--
+
+CREATE TABLE `billing` (
+  `billID` int(11) NOT NULL,
+  `reservationID` int(11) NOT NULL,
+  `stayAmount` decimal(10,2) NOT NULL,
+  `serviceAmount` decimal(10,2) NOT NULL,
+  `totalAmount` decimal(10,2) NOT NULL,
+  `billDate` datetime NOT NULL,
+  `paymentStatus` varchar(20) NOT NULL,
+  `employeeID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customerID` int(11) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `street` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `province` varchar(50) DEFAULT NULL,
+  `zip` varchar(10) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `isExisting` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `employeeID` int(11) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `positionID` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `image` mediumblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `floor`
+--
+
+CREATE TABLE `floor` (
+  `floorID` int(11) NOT NULL,
+  `floorName` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `position`
+--
+
+CREATE TABLE `position` (
+  `positionID` int(11) NOT NULL,
+  `positionName` varchar(50) NOT NULL,
+  `accessLevel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `reservationID` int(11) NOT NULL,
+  `customerID` int(11) NOT NULL,
+  `roomNumber` varchar(10) NOT NULL,
+  `checkinDate` date NOT NULL,
+  `checkoutDate` date NOT NULL,
+  `arrivalTime` time DEFAULT NULL,
+  `numberOfGuests` int(11) NOT NULL,
+  `totalStayBill` decimal(10,2) NOT NULL,
+  `depositPaid` decimal(10,2) NOT NULL,
+  `isConfirmed` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `roomNumber` varchar(10) NOT NULL,
+  `roomType` varchar(50) NOT NULL,
+  `rate` decimal(10,2) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `floorID` int(11) NOT NULL
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `serviceID` int(11) NOT NULL,
+  `serviceName` varchar(100) NOT NULL,
+  `serviceType` varchar(50) NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_order`
+--
+
+CREATE TABLE `service_order` (
+  `serviceOrderID` int(11) NOT NULL,
+  `reservationID` int(11) NOT NULL,
+  `serviceID` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `orderDate` datetime NOT NULL,
+  `subTotal` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`billID`),
+  ADD KEY `reservationID` (`reservationID`),
+  ADD KEY `employeeID` (`employeeID`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customerID`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`employeeID`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `positionID` (`positionID`);
+
+--
+-- Indexes for table `floor`
+--
+ALTER TABLE `floor`
+  ADD PRIMARY KEY (`floorID`),
+  ADD UNIQUE KEY `floorName` (`floorName`);
+
+--
+-- Indexes for table `position`
+--
+ALTER TABLE `position`
+  ADD PRIMARY KEY (`positionID`),
+  ADD UNIQUE KEY `positionName` (`positionName`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`reservationID`),
+  ADD KEY `roomNumber` (`roomNumber`),
+  ADD KEY `customerID` (`customerID`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`roomNumber`),
+  ADD KEY `floorID` (`floorID`);
+
+--
+-- Indexes for table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`serviceID`),
+  ADD UNIQUE KEY `serviceName` (`serviceName`);
+
+--
+-- Indexes for table `service_order`
+--
+ALTER TABLE `service_order`
+  ADD PRIMARY KEY (`serviceOrderID`),
+  ADD KEY `reservationID` (`reservationID`),
+  ADD KEY `serviceID` (`serviceID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `billID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customerID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `employeeID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `floor`
+--
+ALTER TABLE `floor`
+  MODIFY `floorID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `position`
+--
+ALTER TABLE `position`
+  MODIFY `positionID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `reservationID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `service`
+--
+ALTER TABLE `service`
+  MODIFY `serviceID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `service_order`
+--
+ALTER TABLE `service_order`
+  MODIFY `serviceOrderID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `billing`
+--
+ALTER TABLE `billing`
+  ADD CONSTRAINT `billing_ibfk_1` FOREIGN KEY (`reservationID`) REFERENCES `reservation` (`reservationID`),
+  ADD CONSTRAINT `billing_ibfk_2` FOREIGN KEY (`employeeID`) REFERENCES `employee` (`employeeID`);
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`positionID`) REFERENCES `position` (`positionID`);
+
+--
+-- Constraints for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`roomNumber`) REFERENCES `room` (`roomNumber`),
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`customerID`) REFERENCES `customer` (`customerID`);
+
+--
+-- Constraints for table `room`
+--
+ALTER TABLE `room`
+  ADD CONSTRAINT `room_ibfk_1` FOREIGN KEY (`floorID`) REFERENCES `floor` (`floorID`);
+
+--
+-- Constraints for table `service_order`
+--
+ALTER TABLE `service_order`
+  ADD CONSTRAINT `service_order_ibfk_1` FOREIGN KEY (`reservationID`) REFERENCES `reservation` (`reservationID`),
+  ADD CONSTRAINT `service_order_ibfk_2` FOREIGN KEY (`serviceID`) REFERENCES `service` (`serviceID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
